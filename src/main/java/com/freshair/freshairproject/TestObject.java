05e383307da3290015873541ff0631c59d3ffca4
wbadmin <>
2023-05-30 13:07:12 +0200

    {/src/main/java/com/freshair/freshairproject/TestObject.java}

--- src/main/java/com/freshair/freshairproject/TestObject.java
+++ src/main/java/com/freshair/freshairproject/TestObject.java
@@ -6,12 +6,23 @@ package com.freshair.freshairproject;
 
 public class TestObject implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public TestObject() {
-    }
+	private java.lang.String name;
 
+	public TestObject() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public TestObject(java.lang.String name) {
+		this.name = name;
+	}
 
 }
